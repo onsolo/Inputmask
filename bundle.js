5896f0bd9c59fdddc2b7a017b7d43b6468c17e4f
nsolo <smetannikovnikita@yandex.ru>
2024-06-14 12:39:03 +0300

    custom element cut out

--- bundle.js
+++ bundle.js
@@ -6,7 +6,6 @@ import "./lib/polyfills/String.includes";
 import "./lib/extensions/inputmask.extensions";
 import "./lib/extensions/inputmask.date.extensions";
 import "./lib/extensions/inputmask.numeric.extensions";
-import "./lib/inputmaskElement";
 import Inputmask from "./lib/inputmask";
 
 export default Inputmask;
